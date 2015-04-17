2648069006df8be81c9b13d70e44f8c079a45748
memamdie <memamdie@ufl.edu>
2015-04-16 23:52:37 -0400

    Cleaned up code and created print methods that will be deleted before submitting

--- main.cpp
+++ main.cpp
@@ -2,43 +2,39 @@
 //  main.cpp
 //  FloydWarshall
 //
-//  Created by Michelle Emamdie on 4/15/15.
-//  Copyright (c) 2015 Michelle Emamdie. All rights reserved.
-//
 
 #include <iostream>
 #include <sstream>
 #include <vector>
-
 using namespace std;
 
-//int nodes, edges, queries, x, y, cost;
-//vector< vector<int> > graph(nodes+1, vector<int>(nodes+1));
-
-/*void print(){
+void printCost(vector<vector<int> >& graph, int nodes){
 	//Print out the graph to make sure it is entered correctly
+    cout << "This is the cost matrix:\n";
 	for (int i = 1; i < nodes; i++) {
 		for (int j = 1; j < nodes; j++) {
 			cout << graph[i][j] << "\t";
 		}
 		cout << endl;
 	}
-} */
-
+}
+void printPath(vector<vector<string> >& path, int nodes){
+    cout << "This is the path matrix:\n";
+    for (int i = 1; i < nodes; i++) {
+        for (int j = 1; j < nodes; j++) {
+            if(path[i][j].empty()) cout << "NONE" << "\t\t\t";
+            else cout << path[i][j] << "\t\t\t";
+        }
+        cout << endl;
+    }
+}
 int main(int argc, const char * argv[]) {
 	int nodes, edges, queries, x, y, cost, start, end;
-	//cout << "How many nodes would you like to create?" << endl;
 	cin >> nodes >> edges >> queries;
 	nodes++;
 	vector< vector<int> > graph(nodes + 1, vector<int>(nodes + 1));
     vector< vector<string> > path(nodes + 1, vector<string>(nodes + 1));
-	//cout << "How many edges would you like to create?" << endl;
-	//cin >> edges;
-	//cout << "How many queries would you like to create?" << endl;
-	//cin >> queries;
-	int count = 0;
-	//Set the costs at the right position
-	while (count < edges) {
+    for (int count = 0; count < edges; count++) {
         stringstream ss;
 		cin >> x >> y >> cost;
         ss << x << "-" << y;
@@ -46,19 +42,6 @@ int main(int argc, const char * argv[]) {
         path[x][y] = ss.str();
 		count++;
 	}
-	cout << "\n\n\nThe array after being initialized is:\n";
-							 
-	//print();
-	//Print out the graph to make sure it is entered correctly
-	for (int i = 1; i < nodes; i++) {
-		for (int j = 1; j < nodes; j++) {
-			cout << graph[i][j] << "\t";
-		}
-		cout << endl;
-	}
-	cout << "\n\n\n";
-
-	int num_queries = 0;
 	for (int k = 0; k < 7; k++){
 		for (int i = 0; i < 7; i++){
 			for (int j = 0; j < 7; j++){
@@ -89,25 +72,7 @@ int main(int argc, const char * argv[]) {
 		graph[i][i] = 0;
         path[i][i] = ss.str();
 	}
-	cout << "\n\n\nThe distance matrix contains:\n"<<endl;
-	//print();
-	//Print out the graph to make sure it is entered correctly
-    /*for (int i = 1; i < nodes; i++) {
-		for (int j = 1; j < nodes; j++) {
-			cout << graph[i][j] << "\t";
-		}
-		cout << endl;
-	}
-    cout << "\n\nThe path matrix containts:\n" << endl;
-    for (int i = 1; i < nodes; i++) {
-        for (int j = 1; j < nodes; j++) {
-            if(path[i][j].empty()) cout << "NONE" << "\t\t\t";
-            else cout << path[i][j] << "\t\t\t";
-        }
-        cout << endl;
-    }*/
-	//Now we can start to check out what queries the user has
-	while (num_queries < queries) {
+    for (int num_queries = 0; num_queries < queries; num_queries++) {
 		cin >> start >> end;
 		if (path[start][end].empty()) {
 			cout << "NO PATH" << endl;
@@ -115,7 +80,8 @@ int main(int argc, const char * argv[]) {
 		else {
 			cout << "cost = " << graph[start][end] << endl << path[start][end] << endl;
 		}
-		num_queries++;
 	}
+    //printCost(graph, nodes);
+    //printPath(path, nodes);
     return 0;
 }
